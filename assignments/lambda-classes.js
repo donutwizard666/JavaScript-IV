7d644e97654faa67439c2037ad31d8ef9d7efb41
donutwizard666 <thebeardedwarrior@icloud.com>
2019-09-18 22:49:10 -0700

    made person class and started on instructor class

--- assignments/lambda-classes.js
+++ assignments/lambda-classes.js
@@ -1,1 +1,34 @@
 // CODE here for your Lambda Classes
+
+// === Person Class === //
+
+class Person {
+    constructor(attributes) {
+        this.name = attributes.name;
+        this.age = attributes.age;
+        this.location = attributes.location;
+    }
+    speak() {
+        console.log(`Hello, my name is ${this.name}, I am from ${this.location}`);
+    }
+}
+
+// === Instructor Class ===
+
+class Instructor extends Person {
+    constructor(attributes) {
+        super(attributes);
+        this.specialty = attributes.specialty;
+        this.favLanguage = attributes.favLanguage;
+        this.catchPhrase = attributes.catchPhrase;
+    }
+    demo(subject) {
+        console.log(`Today we are learning about ${subject}`);
+    }
+
+
+
+
+
+
+}
